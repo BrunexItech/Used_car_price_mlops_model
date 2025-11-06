b2924fc01fb015fca1bf244f6a3deee59b59cea9
BrunexItech <brunosharif89@gmail.com>
2025-11-06 20:21:17 +0300

    Fix setup.py parameter names

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 with open('requirements.txt','r', encoding='utf-8') as fh:
-    requirements=[line.strip() for line in fh if line.strip() and not line.startwith('#')]
+    requirements=[line.strip() for line in fh if line.strip() and not line.startswith('#')]
     
     setup(
         name='car_price_predictor',
@@ -10,10 +10,10 @@ with open('requirements.txt','r', encoding='utf-8') as fh:
         authr_email='brunosharif89@gmail.com',
         description='A MLOPS project for car price prediction',
         long_description='An end to end MLOPS project for predicting car prices using structured vehicle data',
-        long_description_content='text/markdown',
+        long_description_content_type='text/markdown',
         packages=find_packages(where='src'),
         package_dir={'':'src'},
-        python_requirements='>=3.8',
+        python_requires='>=3.8',
         install_requires=requirements,
         classifiers=[
         "Development Status :: 3 - Alpha",
